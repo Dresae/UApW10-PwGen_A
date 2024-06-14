c0e466522330a0c9e88a93089b24f4033a9b93c4
lvdev <lov3r.dev@gmail.com>
2024-06-14 01:06:30 +0100

    adding const in js file

---
+++ z.js
@@ -0,0 +1,6 @@
+const lengthSlider = document.querySelector(".pass-length input");
+const options = document.querySelectorAll(".options input");
+const copyIcon =document.querySelector(".input-box span");
+const passwordInput = document.querySelector(".input-box input");
+const passIndicator =document.querySelector(".pass-indicator")
+const generateButton = document.querySelector(".generate-btn");
