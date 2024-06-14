f5df50fc941c0e7763a27509e4d91d5b0b915fb0
lvdev <lov3r.dev@gmail.com>
2024-06-14 02:33:23 +0100

    generate password constant in js

--- z.js
+++ z.js
@@ -4,3 +4,41 @@ const copyIcon =document.querySelector(".input-box span");
 const passwordInput = document.querySelector(".input-box input");
 const passIndicator =document.querySelector(".pass-indicator")
 const generateButton = document.querySelector(".generate-btn");
+
+const characters = {
+    lowercase: "abcdefghijklmnopqrstuvwxyz",
+    uppercase: "ABCDEFGHIJKLMNOPQRSTUVWXYZ",
+    numbers: "0123456789",
+    symbols: "!$%&|[](){}:;.,*+-#@<>~"
+}
+
+
+const generatePassword = () => {
+    let staticPassword = "",
+        ramdomPassword = "",
+        excludeDuplicate = false,
+        passLength = lengthSlider.value;
+
+    options.forEach(option => {
+        if (option.checked) {
+            if (option.id !== "exc-duplicate" && option.id !== "spaces") {
+                staticPassword += characters[option.id];
+            } else if (option.id === "spaces") {
+                staticPassword += `  ${staticPassword}  `;
+            } else {
+                excludeDuplicate = true;
+            }
+        }
+    });
+
+    for (let i = 0; i < passLength; i++) {
+        let ramdonChar = staticPassword[Math.floor(Math.ramdom() * staticPassword.length)];
+        if (excludeDuplicate) {
+            !randomPassword.includes(randomChar) || randomChar == " " ? randomPassword += randomChar : i--;
+        } else {
+            randomPassword += randomChar;
+        }
+    }
+    passwordInput.value = randomPassword;
+}
+
